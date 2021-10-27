cb34f9b457936f9084cdaa45635869d2b166e60e
Lucas Cintra <luucascintrag@gmail.com>
2021-10-27 19:50:49 -0300

    exc 1 da lista 2

---
+++ JS/class-04/lista-02/exc1.js
@@ -0,0 +1,20 @@
+function exc2(string) {
+  if(typeof string != 'string'){
+    return 'Parâmetro inválido'
+  }
+  string = string.toLowerCase()
+  stringArray = string.split('')
+  let contadorVogais = 0;
+  let contadorConsoantes = 0;
+  for (let i = 0; i < stringArray.length; i++) {
+    if(stringArray[i].charCodeAt() == 97 || stringArray[i].charCodeAt() == 101 || stringArray[i].charCodeAt() == 105 || stringArray[i].charCodeAt() == 111 || stringArray[i].charCodeAt() == 117){
+        contadorVogais++
+    } else if(stringArray[i].charCodeAt()>=97 && stringArray[i].charCodeAt()<=122){
+        contadorConsoantes++
+    }
+  }
+  return `A frase ${string} tem ${contadorVogais} vogais e ${contadorConsoantes} consoantes`;
+}
+
+let stringExemplo = 'uma string de exemplo 320320 @!#|';
+console.log(exc2(stringExemplo));
