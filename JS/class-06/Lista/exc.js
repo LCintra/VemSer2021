0e930c33edd16ad23a015cf0fc7f4b71bd145cf3
Lucas Cintra <luucascintrag@gmail.com>
2021-11-01 14:14:48 -0300

    Refatorando o código e adicionando verificação de caracteres especiais

--- JS/class-06/Lista/exc.js
+++ JS/class-06/Lista/exc.js
@@ -20,6 +20,17 @@ class Validacoes{
             return true;
         }
     }
+    validaApenasLetras(string){
+        let stringSemEspacos = string.replaceAll(' ', '');
+        let stringArray = [...stringSemEspacos];
+        let stringArrayEhInvalido = stringArray.some(caractere => caractere.toLowerCase() === caractere.toUpperCase())
+        if(stringArrayEhInvalido){
+            alert('Foi inserido caracteres inválidos!')
+            return true
+        } else {
+            return false
+        }
+    }
     validaIdExiste(id,array){
         if(array.some(dado => dado.id === id)){
             return true;
@@ -84,6 +95,106 @@ class Projeto{
     }
 }
 
+const criarColaborador = nome =>{
+    //criando o colaborador
+    colaboradores.push(new Colaborador(contadorIdColaboradores,nome));
+    ///avisando o sucesso na criação
+    alert(`Colaborador ${nome} de ID ${contadorIdColaboradores} cadastrado com sucesso!`);
+    //incrementando o id pro próximo colaborador
+    contadorIdColaboradores++;
+    console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
+}
+
+const criarProjeto = titulo =>{
+    //Criando o projeto
+    projetos.push(new Projeto(contadorIdProjetos,titulo));
+    //avisando o sucesso na criação
+    alert(`Projeto ${titulo} de ID ${contadorIdProjetos} cadastrado com sucesso!`);
+    //incrementando o id pro proximo projeto
+    contadorIdProjetos++;
+    console.log('\nConsole de cada elemento da lista => ', ...projetos);
+}
+
+const alocarColaborador = (idColaboradorAlocado,idProjetoAlocar) =>{
+    //pegando o projeto do id informado
+    let projeto = projetos.find(projeto => projeto.id == idProjetoAlocar);
+    //pegando o colaborador do id informado
+    let colaborador = colaboradores.find(colaborador => colaborador.id == idColaboradorAlocado);
+    //inserindo o colaborador no array do projeto
+    projeto.colaboradoresAlocados.push(colaborador);
+    //inserindo no colaborador qual o projeto que ele está
+    colaborador.codProjeto = idProjetoAlocar;
+    alert(`O colaborador ${colaborador.nome} foi alocado do projeto ${projeto.titulo}`);
+    console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
+    console.log('\nConsole de cada elemento da lista => ', ...projetos);
+}
+
+const desalocarColaborador = idColaboradorADesalocar =>{
+    //pegando as informações do projeto e do colaborador
+    let colaboradorADesalocar = colaboradores.find(colaborador => colaborador.id == idColaboradorADesalocar);
+    let projetoADesalocar = projetos.find(projeto => projeto.id == colaboradorADesalocar.codProjeto);
+    //confirmação
+    if(confirm(`Tem certeza que deseja desalocar o colaborador ${colaboradorADesalocar.nome} de ID ${colaboradorADesalocar.id} do projeto ${projetoADesalocar.titulo} de ID ${projetoADesalocar.id}?`)){
+        //retirando o projeto da classe do colaborador
+        colaboradorADesalocar.codProjeto = 0;
+        //retirando o colaborador do array do projeto
+        projetoADesalocar.colaboradoresAlocados = projetoADesalocar.colaboradoresAlocados.filter(colaborador => colaborador.id != colaboradorADesalocar.id);
+        alert('Colaborador desalocado!');
+    }
+    console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
+    console.log('\nConsole de cada elemento da lista => ', ...projetos);
+}
+
+const mostrarColaboradoresSemProjeto = () =>{
+    //criando a mensagem
+    let messageColaboradorSemprojeto = 'Lista de Colaboradores sem projeto alocado: \n';
+    //filtrando os colaboradores sem projeto
+    let colaboradoresSemProjeto = colaboradores.filter(colaboradorFiltrar => colaboradorFiltrar.codProjeto == 0);
+    //iterando sobre o array para criar a mensagem com os dados
+    for(object of colaboradoresSemProjeto){
+        messageColaboradorSemprojeto += 'Colaborador de nome ' + object.nome + ' e ID ' + object.id + '\n';
+    }
+    //exibindo a mensagem
+    alert(messageColaboradorSemprojeto);
+}
+
+const mostrarProjetosSemColaboradores = () => {
+    //criando a mensagem
+    let messageProjetoSemColaborador = 'Lista de Projetos sem nenhum colaborador alocado: \n';
+    // filtrando os projetos sem colaboradores
+    let projetosSemColaboradores = projetos.filter(projetoFiltrar => projetoFiltrar.colaboradoresAlocados.length == 0);
+    //iterando sobre o array para criar a mensagem com os dados
+    for(object of projetosSemColaboradores){
+        messageProjetoSemColaborador += 'Projeto de nome ' + object.titulo + ' e ID ' + object.id + '\n';
+    }
+    //exibir a mensagem
+    alert(messageProjetoSemColaborador);
+}
+
+const mostrarColaboradoresSemPonto = () =>{
+    //criando a mensagem
+    let messageColaboradorSemPonto = 'Lista de Colaboradores sem ponto marcado: \n';
+    //filtrando os colaboradores sem ponto
+    let colaboradoresSemPonto = colaboradores.filter(colaborador => colaborador.marcacoesPonto.length == 0);
+    //iterando sobre o array para criar a mensagem com os dados
+    for(object of colaboradoresSemPonto){
+        messageColaboradorSemPonto += 'Colaborador de nome ' + object.nome + ' e ID ' + object.id + '\n';
+    }
+    //exibindo a mensagem
+    alert(messageColaboradorSemPonto);
+}
+
+const marcarPonto = (idColaboradorMarcarPonto,horaPonto,diaPonto) =>{
+    //pegando as informações do colaborador
+    let colaboradorAMarcarPonto = colaboradores.find(colaborador => colaborador.id == idColaboradorMarcarPonto);
+    if(confirm(`Confirma marcar o ponto de ${colaboradorAMarcarPonto.nome} no dia ${diaPonto} as ${horaPonto} horas?`)){
+        colaboradorAMarcarPonto.marcarPonto(horaPonto,diaPonto);
+        alert('Ponto marcado!');
+    }
+    console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
+    console.log('\nConsole de cada elemento da lista => ', ...projetos);
+}
+
 do{
     var resp = prompt(` Sistema de projetos e alocações \n 
     1 - Cadastrar Colaborador \n 
@@ -100,26 +211,14 @@ do{
         case '1':
             //pedindo o nome do colaborador
             let nome = prompt('Qual o nome do colaborador?');
-            if(!new Validacoes().validaIsNotEmpty(nome)) break;
-            //criando o colaborador
-            colaboradores.push(new Colaborador(contadorIdColaboradores,nome));
-            ///avisando o sucesso na criação
-            alert(`Colaborador ${nome} de ID ${contadorIdColaboradores} cadastrado com sucesso!`);
-            //incrementando o id pro próximo colaborador
-            contadorIdColaboradores++;
-            console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
+            if(!new Validacoes().validaIsNotEmpty(nome) || new Validacoes().validaApenasLetras(nome)) break;
+            criarColaborador(nome);
             break;
         case '2':
             //pedindo o titulo do projeto
             let titulo = prompt('Qual o título do projeto?');
-            if(!new Validacoes().validaIsNotEmpty(titulo)) break;
-            //Criando o projeto
-            projetos.push(new Projeto(contadorIdProjetos,titulo));
-            //avisando o sucesso na criação
-            alert(`Projeto ${titulo} de ID ${contadorIdProjetos} cadastrado com sucesso!`);
-            //incrementando o id pro proximo projeto
-            contadorIdProjetos++;
-            console.log('\nConsole de cada elemento da lista => ', ...projetos);
+            if(!new Validacoes().validaIsNotEmpty(titulo) || new Validacoes().validaApenasLetras(titulo)) break;
+            criarProjeto(titulo);
             break;
         case '3':
             //pedindo o id do colaborador
@@ -134,17 +233,7 @@ do{
             idProjetoAlocar = parseFloat(idProjetoAlocar);
             //verificações
             if(!new Validacoes().validaIsNumber(idProjetoAlocar) || !new Validacoes().validaIdExiste(idProjetoAlocar,projetos)) break;
-            //pegando o projeto do id informado
-            let projeto = projetos.find(projeto => projeto.id == idProjetoAlocar);
-            //pegando o colaborador do id informado
-            let colaborador = colaboradores.find(colaborador => colaborador.id == idColaboradorAlocado);
-            //inserindo o colaborador no array do projeto
-            projeto.colaboradoresAlocados.push(colaborador);
-            //inserindo no colaborador qual o projeto que ele está
-            colaborador.codProjeto = idProjetoAlocar;
-            alert(`O colaborador ${colaborador.nome} foi alocado do projeto ${projeto.titulo}`);
-            console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
-            console.log('\nConsole de cada elemento da lista => ', ...projetos);
+            alocarColaborador(idColaboradorAlocado,idProjetoAlocar);
             break;
         case '4':
             //pedindo o id do colaborador que deseja desalocar  
@@ -153,76 +242,32 @@ do{
             if(!new Validacoes().validaIsNumber(idColaboradorADesalocar) ||
             !new Validacoes().validaIdExiste(idColaboradorADesalocar,colaboradores) ||
             !new Validacoes().validaColaboradorPodeDesalocar(idColaboradorADesalocar)) break;
-            //pegando as informações do projeto e do colaborador
-            let colaboradorADesalocar = colaboradores.find(colaborador => colaborador.id == idColaboradorADesalocar);
-            let projetoADesalocar = projetos.find(projeto => projeto.id == colaboradorADesalocar.codProjeto);
-            //confirmação
-            if(confirm(`Tem certeza que deseja desalocar o colaborador ${colaboradorADesalocar.nome} de ID ${colaboradorADesalocar.id} do projeto ${projetoADesalocar.titulo} de ID ${projetoADesalocar.id}?`)){
-                //retirando o projeto da classe do colaborador
-                colaboradorADesalocar.codProjeto = 0;
-                //retirando o colaborador do array do projeto
-                projetoADesalocar.colaboradoresAlocados = projetoADesalocar.colaboradoresAlocados.filter(colaborador => colaborador.id != colaboradorADesalocar.id);
-                alert('Colaborador desalocado!');
-            }
-            console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
-            console.log('\nConsole de cada elemento da lista => ', ...projetos);
-            break
+            desalocarColaborador(idColaboradorADesalocar);
+            break;
         case '5':
             //pedindo o id do colaborador que vai marcar o ponto
             let idColaboradorMarcarPonto = parseFloat(prompt('Qual o id do colaborador que irá marcar o ponto?'));
-            //pegando as informações do colaborador
-            let colaboradorAMarcarPonto = colaboradores.find(colaborador => colaborador.id == idColaboradorMarcarPonto);
             //verificações
             if(!new Validacoes().validaIsNumber(idColaboradorMarcarPonto) ||
             !new Validacoes().validaIdExiste(idColaboradorMarcarPonto,colaboradores)) break;
             //pedindo a hora
             let horaPonto = parseFloat(prompt('Qual a hora do ponto?'));
+            //validações
             if(!new Validacoes().validaIsNumber(horaPonto)) break;
             //pedindo o dia
             let diaPonto = parseFloat(prompt('Qual o dia do ponto?'));
+            //validações
             if(!new Validacoes().validaIsNumber(diaPonto)) break;
-            if(confirm(`Confirma marcar o ponto de ${colaboradorAMarcarPonto.nome} no dia ${diaPonto} as ${horaPonto} horas?`)){
-                colaboradorAMarcarPonto.marcarPonto(horaPonto,diaPonto);
-                alert('Ponto marcado!');
-            }
-            console.log('\nConsole de cada elemento da lista => ', ...colaboradores);
-            console.log('\nConsole de cada elemento da lista => ', ...projetos);
-            break
+            marcarPonto(idColaboradorMarcarPonto,horaPonto,diaPonto);
+            break;
         case '6':
-            //criando a mensagem
-            let messageColaboradorSemprojeto = 'Lista de Colaboradores sem projeto alocado: \n';
-            //filtrando os colaboradores sem projeto
-            let colaboradoresSemProjeto = colaboradores.filter(colaboradorFiltrar => colaboradorFiltrar.codProjeto == 0);
-            //iterando sobre o array para criar a mensagem com os dados
-            for(object of colaboradoresSemProjeto){
-                messageColaboradorSemprojeto += 'Colaborador de nome ' + object.nome + ' e ID ' + object.id + '\n';
-            }
-            //exibindo a mensagem
-            alert(messageColaboradorSemprojeto);
+            mostrarColaboradoresSemProjeto();
             break;
         case '7':
-            //criando a mensagem
-            let messageProjetoSemColaborador = 'Lista de Projetos sem nenhum colaborador alocado: \n';
-            // filtrando os projetos sem colaboradores
-            let projetosSemColaboradores = projetos.filter(projetoFiltrar => projetoFiltrar.colaboradoresAlocados.length == 0);
-            //iterando sobre o array para criar a mensagem com os dados
-            for(object of projetosSemColaboradores){
-                messageProjetoSemColaborador += 'Projeto de nome ' + object.titulo + ' e ID ' + object.id + '\n';
-            }
-            //exibir a mensagem
-            alert(messageProjetoSemColaborador);
-            break
+            mostrarProjetosSemColaboradores();
+            break;
         case '8':
-            //criando a mensagem
-            let messageColaboradorSemPonto = 'Lista de Colaboradores sem ponto marcado: \n';
-            //filtrando os colaboradores sem ponto
-            let colaboradoresSemPonto = colaboradores.filter(colaborador => colaborador.marcacoesPonto.length == 0);
-            //iterando sobre o array para criar a mensagem com os dados
-            for(object of colaboradoresSemPonto){
-                messageColaboradorSemPonto += 'Colaborador de nome ' + object.nome + ' e ID ' + object.id + '\n';
-            }
-            //exibindo a mensagem
-            alert(messageColaboradorSemPonto);
+            mostrarColaboradoresSemPonto();
             break;
         case '9':
             //mensagem antes de desligar o prompt
@@ -235,5 +280,3 @@ do{
             alert('Opção Inválida - Caso queira desligar o sistema pressione 9');
     }
 } while(resp != 9)
-
-
